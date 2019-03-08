317d40e20ed41d73cf04058f261d60cb7cbc3074
yuya.kudo <yuyaaa.1203@gmail.com>
2019-03-08 03:10:41 +0000

    ranking

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     delete 'logout', to: 'sessions#destroy'
     
     get 'signup', to: 'users#new'
+    
+    get 'rankings/want', to: 'rankings#want'
+    
     resources :users, only: [:show, :new, :create]
     
     resources :items, only: [:show, :new]
